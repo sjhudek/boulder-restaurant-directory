916e824e31e1b7e2dee74f24af140d105d946650
Steve Hudek <sjhudek@gmail.com>
2017-12-12 14:22:48 -0700

    Added ratings stars to info window. Left column next to map.

--- public/js/main.js
+++ public/js/main.js
@@ -5,59 +5,81 @@ $("#displayListing").toggle();
 
 function initMap (){
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 40.0149856,
-            lng: -105.2705456
-        },
-        zoom: 15,
-        mapTypeId: 'roadmap',
-        mapTypeControl: true,
-        mapTypeControlOptions: {
-            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-            position: google.maps.ControlPosition.LEFT_TOP
-        }
-    }, {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 40.0149856,
+      lng: -105.2705456
+    },
+    zoom: 15,
+    mapTypeId: 'roadmap',
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      position: google.maps.ControlPosition.LEFT_TOP
+    }
+  }, {
 
-        zoomControl: true,
-        zoomControlOptions: {
-            position: google.maps.ControlPosition.LEFT_CENTER
-        },
-        scaleControl: true,
-        streetViewControl: true,
-        streetViewControlOptions: {
-            position: google.maps.ControlPosition.LEFT_TOP
-        },
-        fullscreenControl: true
-    });
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-      location: {
-         lat: 40.0149856,
-         lng: -105.2705456
-      },
-      radius: 500,
-      type: ['store']
-    }, callback);
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.LEFT_CENTER
+    },
+    scaleControl: true,
+    streetViewControl: true,
+    streetViewControlOptions: {
+      position: google.maps.ControlPosition.LEFT_TOP
+    },
+    fullscreenControl: true
+  });
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: {
+     lat: 40.0149856,
+     lng: -105.2705456
+   },
+   radius: 500,
+   type: ['store']
+ }, callback);
 };
 
 function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
     }
   }
+}
 
-  function createMarker(place) {
-    console.log(place)
+function createListItem(place) {
+  console.log(place);
+  var stars = '';
+  for (let i = 0; i < Math.round(place.rating); i ++) {
+    stars += '&star;'
+  }
+  var content = `
+  <div> 
+  <p>${place.name}</p>
+  <p>${place.googleAddress}</p>
+  <p>Rating: ${place.rating} ${stars}</p>
+  </div>  
+  <hr/>
+  `
+
+  $('#resultList').append(content);
+}
+
+function createMarker(place) {
+  // console.log(place)
     // build html string for content of infoWindow
+    var stars = '';
+    for (let i = 0; i < Math.round(place.rating); i ++) {
+      stars += '&star;'
+    }
     var content = `
     <div> 
-        <p>${place.name}</p>
-        <p>${place.googleAddress}</p>
-        <p>Rating: ${place.rating}</p>
+    <p>${place.name}</p>
+    <p>${place.googleAddress}</p>
+    <p>Rating: ${place.rating} ${stars}</p>
     </div>
     `;
 
@@ -73,63 +95,61 @@ function callback(results, status) {
     marker.addListener('click', toggleBounce);
 
     function toggleBounce() {
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
+      if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+      } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
       }
+    }
 
     google.maps.event.addListener(marker, 'click', function() {
       infowindow.setContent(content);
       infowindow.open(map, this);
     });
 
+    function showInfoWindow() {
+     var marker = this;
+     places.getDetails({
+       placeId: marker.placeResult.place_id
+     },
+     function(place, status) {
+       if (status !== google.maps.places.PlacesServiceStatus.OK) {
+         return;
+       }
+       infoWindow.open(map, marker);
+       buildIWContent(place);
+     });
+   }
+ };
+
+ $('#searchBtn').on('click', function (event) {
+  event.preventDefault();
+
+  var searchWhatToDo = $('#whatToDo').val();
+  var searchPlaceToGo = $('#place').val();
+
+  var searchTerm = {
+    searchWhat: searchWhatToDo,
+    searchPlace: searchPlaceToGo
+  }
 
 
-  };
-
-
-
-$('#searchBtn').on('click', function (event) {
-    event.preventDefault();
-
-    var searchWhatToDo = $('#whatToDo').val();
-    var searchPlaceToGo = $('#place').val();
-
-    var searchTerm = {
-        searchWhat: searchWhatToDo,
-        searchPlace: searchPlaceToGo
-    }
-
-
-    var resultsArr = [];
-    $.post('/search', searchTerm, (dataFromServer) => {
-        console.log(dataFromServer)
-        dataFromServer.forEach(function(element){
-            createMarker(element);
-        })
-        // resultsArr = dataFromServer.map(function (element, index) {
-
-        //     if (index === 0) {
-        //         map.setZoom(15);
-        //         map.setCenter(element.location);
-        //     }
-
-        //     return (new google.maps.Marker({
-        //         position: element.location,
-        //         map: map
-        //     }));
-
-        });
-    });
-
-
-    $("#displayListing").toggle();
-
-    $(document).ready(function() {
-        var inpPlace = document.getElementById('place')
-
-        var autocomplete = new google.maps.places.Autocomplete(inpPlace);
-        autocomplete.bindTo('bounds', map);
+  var resultsArr = [];
+  $.post('/search', searchTerm, (dataFromServer) => {
+    console.log(dataFromServer)
+    dataFromServer.forEach(function(element){
+      createMarker(element);
+      createListItem(element);
     })
+  });
+});
+
+
+ $("#displayListing").toggle();
+
+ $(document).ready(function() {
+  var inpPlace = document.getElementById('place')
+
+  var autocomplete = new google.maps.places.Autocomplete(inpPlace);
+  autocomplete.bindTo('bounds', map);
+})
